22684397b7c9d716de79f8320758cfc136f03095
TPLinux <tplinux@localhost.localdomain>
2017-10-11 16:19:11 -0400

    updates

--- app/src/main/java/sa/sarah/nationalambulance/UserMainActivity.kt
+++ app/src/main/java/sa/sarah/nationalambulance/UserMainActivity.kt
@@ -1,12 +1,19 @@
 package sa.sarah.nationalambulance
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_user_main.*
 
 class UserMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_main)
+
+        open_map_btn.setOnClickListener({
+            val mapIntent = Intent(this, UserMapActivity::class.java)
+            startActivity(mapIntent)
+        })
     }
 }
